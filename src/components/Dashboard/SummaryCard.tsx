497cc775f473df0dd7d7ba42a4f6731f1b1ee35c
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-04-15 14:30:33 +0530

    feat: add dynamic summary calculations from transactions

--- src/components/Dashboard/SummaryCard.tsx
+++ src/components/Dashboard/SummaryCard.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useTransaction } from "../../hooks/useTransaction";
 
-const SummaryCard = ({ n }: { n: number }) => {
+type summuryCardData = {
+  title: string;
+  amount: number;
+  percentageChange?: number;
+};
+
+const SummaryCard = ({
+  title,
+  amount,
+  percentageChange = 0,
+}: summuryCardData) => {
   return (
     <div className="inline-flex w-full flex-col gap-4 rounded-4xl border border-[var(--border-default)] p-2 pb-4 pl-4">
       {/* heading link */}
       <div className="flex justify-between">
-        <h3 className="pt-1 text-2xl font-semibold capitalize">
-          total balance {n}
-        </h3>
+        <h3 className="pt-1 text-2xl font-semibold capitalize">{title}</h3>
         <button className="cursor-pointer rounded-full border border-[var(--border-default)] p-2">
           <svg
             className="fill-[var(--text-primary)]"
@@ -25,11 +34,15 @@ const SummaryCard = ({ n }: { n: number }) => {
       {/* amount message */}
       <div className="flex flex-col gap-6">
         <p className="text-4xl font-bold">
-          ₹15000<span className="text-[var(--text-muted)]">.00</span>
+          {amount}
+          <span className="text-[var(--text-muted)]">.00</span>
         </p>
         <p className="text-[var(--text-secondary)]">
-          <span className="rounded-full bg-[var(--success-bg)] px-1 text-[var(--success)]">
-            12.1%
+          <span
+            className={`rounded-full px-1.5 ${percentageChange > 0 ? (title === "Expense" ? "bg-[var(--danger-bg)] text-[var(--danger)]" : "bg-[var(--success-bg)] text-[var(--success)]") : title === "Expense" ? "bg-[var(--success-bg)] text-[var(--success)]" : "bg-[var(--danger-bg)] text-[var(--danger)]"} `}
+          >
+            {percentageChange > 0 ? "↑ " : "↓ "}
+            {Math.abs(percentageChange)}%
           </span>
           vs last month
         </p>
@@ -39,15 +52,83 @@ const SummaryCard = ({ n }: { n: number }) => {
 };
 
 const SummaryContainer = () => {
+  // getting transaction from the transaction context
+  const { CurrentMonthTransactions, SelectedMonthTransactions } =
+    useTransaction();
+
+  // getting total income for current month
+  const totalIncome = CurrentMonthTransactions.filter((txn) => {
+    return txn["type"] === "Income" ? txn : false;
+  }).reduce((totalIncome, txn) => {
+    return totalIncome + txn["amount"];
+  }, 0);
+
+  // getting total expense for current month
+  const totalExpense = CurrentMonthTransactions.filter((txn) => {
+    return txn["type"] === "Expense" ? txn : false;
+  }).reduce((totalExpense, txn) => {
+    return totalExpense + txn["amount"];
+  }, 0);
+
+  // measureing total balance for current month
+  const totalBalance = totalIncome - totalExpense;
+
+  // getting total income for current month
+  const prevMonthTotalIncome = SelectedMonthTransactions.filter((txn) => {
+    return txn["type"] === "Income" ? txn : false;
+  }).reduce((totalIncome, txn) => {
+    return totalIncome + txn["amount"];
+  }, 0);
+
+  // getting total expense for current month
+  const prevMonthTotalExpense = SelectedMonthTransactions.filter((txn) => {
+    return txn["type"] === "Expense" ? txn : false;
+  }).reduce((totalExpense, txn) => {
+    return totalExpense + txn["amount"];
+  }, 0);
+
+  // measureing total balance for prev month
+  const prevMonthTotalBalance = prevMonthTotalIncome - prevMonthTotalExpense;
+
+  // function to find the status compared with prev month amount
+  const percentageChangeFun = (
+    currentMonth: number,
+    prevMonth: number,
+  ): number => {
+    const percentage = (currentMonth * 100) / prevMonth - 100;
+    return Math.round(percentage * 10) / 10;
+  };
+
   return (
     <div className="grid w-full grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
-        <SummaryCard n={1} />
-        <SummaryCard n={2} />
+        <SummaryCard
+          title="Total Balanace"
+          amount={totalBalance}
+          percentageChange={percentageChangeFun(
+            totalBalance,
+            prevMonthTotalBalance,
+          )}
+        />
+        <SummaryCard
+          title="Income"
+          amount={totalIncome}
+          percentageChange={percentageChangeFun(
+            totalIncome,
+            prevMonthTotalIncome,
+          )}
+        />
       </div>
       <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
-        <SummaryCard n={3} />
-        <SummaryCard n={4} />
+        <SummaryCard
+          title="Expense"
+          amount={totalExpense}
+          percentageChange={percentageChangeFun(
+            totalExpense,
+            prevMonthTotalExpense,
+          )}
+        />
+        <SummaryCard title="" amount={0} />
       </div>
     </div>
   );
