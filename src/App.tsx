b15845cc51dbd8809bc3c85b429b5db7d319f06b
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-04-15 23:04:03 +0530

    feat: add routing configuration in App.tsx for dashboard and transactions pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
 
 const App = () => {
   return (
@@ -11,6 +12,9 @@ const App = () => {
       <Routes>
         {/* dashboard route */}
         <Route path="/" element={<Dashboard />} />
+        
+        {/* transaction route */}
+        <Route path="/transactions" element={<Transactions />} />
       </Routes>
     </div>
   );
