01dd0a10f0bdce1f90565b4fa17864583b2a3347
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-04-15 19:27:21 +0530

    refactor: move dashboard to /dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
-import TopBar from "./layouts/TopBar";
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <div className="grid auto-cols-[auto_1fr] grid-flow-col">
       <Navbar />
-      <Dashboard />
+
+      {/* routing here */}
+      <Routes>
+        {/* dashboard route */}
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 };
