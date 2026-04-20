0b81f33fcdb74d970ace99373bd6b88b8cbaebd0
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-04-20 11:22:46 +0530

    style: add active scale effect and shadow to add transaction button

--- src/components/transaction/Options.tsx
+++ src/components/transaction/Options.tsx
@@ -23,17 +23,17 @@ const Options = ({ selectedDate, setSelectedDate, setIsAdding }: props) => {
       <div className="flex gap-2">
         <button
           onClick={prevMonth}
-          className="cursor-pointer rounded-full border border-[var(--border-default)] p-1 px-3 text-xl font-extrabold text-[var(--text-primary)] shadow-md hover:bg-[var(--accent-subtle)]"
+          className="cursor-pointer rounded-full border border-[var(--border-default)] p-1 px-3 text-xl font-extrabold text-[var(--text-primary)] shadow-md hover:bg-[var(--accent-subtle)] active:scale-97"
         >
           ◂
         </button>
-        <div className="flex w-40 justify-center rounded-full border border-[var(--border-default)] p-2 text-[var(--text-primary)] uppercase shadow-md">
+        <div className="flex w-45 justify-center rounded-full border border-[var(--border-default)] p-2 text-[var(--text-primary)] uppercase shadow-md">
           {selectedDate.toLocaleDateString("default", { month: "long" })} -{" "}
           {selectedDate.getFullYear()}
         </div>
         <button
           onClick={nextMonth}
-          className="cursor-pointer rounded-full border border-[var(--border-default)] p-1 px-3 text-xl font-extrabold text-[var(--text-primary)] shadow-md hover:bg-[var(--accent-subtle)]"
+          className="cursor-pointer rounded-full border border-[var(--border-default)] p-1 px-3 text-xl font-extrabold text-[var(--text-primary)] shadow-md hover:bg-[var(--accent-subtle)] active:scale-97"
         >
           ▸
         </button>
@@ -46,7 +46,7 @@ const Options = ({ selectedDate, setSelectedDate, setIsAdding }: props) => {
           onClick={() => {
             setIsAdding(true);
           }}
-          className="shadow- cursor-pointer rounded-full bg-[var(--accent-primary)] px-5 py-2 text-[var(--text-inverse)]"
+          className="cursor-pointer rounded-full bg-[var(--accent-primary)] px-5 py-2 text-[var(--text-inverse)] shadow-lg active:scale-99"
         >
           + Add New
         </button>
