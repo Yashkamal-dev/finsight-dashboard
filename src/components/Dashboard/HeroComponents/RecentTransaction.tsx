aa4509fe818522d8d1f856ad5144286949a9f8a7
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-05-01 23:24:39 +0530

    style: improve responsiveness of recent transactions component

--- src/components/Dashboard/HeroComponents/RecentTransaction.tsx
+++ src/components/Dashboard/HeroComponents/RecentTransaction.tsx
@@ -16,13 +16,13 @@ const RecentTransaction = () => {
     // transaction container components
     <div className="flex flex-col gap-4 rounded-3xl border border-[var(--border-default)] p-4">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-semibold text-[var(--text-primary)]">
+        <h1 className="md:text-2xl font-semibold text-[var(--text-primary)]">
           Recent Transactions
         </h1>
         <div>
           <Link
             to={"/transactions"}
-            className="cursor-pointer rounded-full border border-[var(--border-default)] px-3 py-1"
+            className="text-sm md:text-base cursor-pointer rounded-full border border-[var(--border-default)] px-3 py-1"
           >
             see all ›
           </Link>
@@ -32,14 +32,16 @@ const RecentTransaction = () => {
       {/* table */}
       <table className="w-full border-spacing-y-3">
         {/* table heading row */}
-        <thead>
+        <thead className=" text-sm md:text-base">
           <tr className="rounded-full bg-[var(--accent-soft)] text-[var(--accent-text)]">
-            <th className="rounded-l-full px-2.5 py-2 text-left font-bold">
+            <th className="hidden rounded-l-full px-2.5 py-2 text-left font-bold md:table-cell">
               Date
             </th>
-            <th className="text-left font-bold">Amount</th>
+            <th className="rounded-l-full px-2.5 py-2 text-left font-bold md:rounded-l-none">
+              Amount
+            </th>
             <th className="text-left font-bold">Payment Name</th>
-            <th className="text-left font-bold">Method</th>
+            <th className="hidden text-left font-bold md:table-cell">Method</th>
             <th className="rounded-r-full text-left font-bold">Category</th>
           </tr>
         </thead>
@@ -51,18 +53,18 @@ const RecentTransaction = () => {
               // table row for each trasnaction in the recentTransactions
               <tr
                 key={crypto.randomUUID()}
-                className={`${txn["type"] === "Income" ? "bg-[var(--success-bg)]" : "bg-[var(--danger-bg)]"}`}
+                className={` text-sm ${txn["type"] === "Income" ? "bg-[var(--success-bg)]" : "bg-[var(--danger-bg)]"}`}
               >
                 {/* date */}
                 <td
-                  className={`rounded-l-full border-b border-[var(--border-default)] px-2 py-1.5`}
+                  className={`hidden rounded-l-full border-b border-[var(--border-default)] px-2 py-1.5 md:table-cell`}
                 >
                   {txn["date"]}
                 </td>
 
                 {/* amount */}
                 <td
-                  className={`border-b border-[var(--border-default)] px-2 py-1.5 text-center`}
+                  className={`rounded-l-full border-b border-[var(--border-default)] px-2 py-1.5 text-center md:rounded-l-none`}
                 >
                   {txn["type"] === "Income" ? "+" : "-"} {txn["amount"]}
                 </td>
@@ -76,7 +78,7 @@ const RecentTransaction = () => {
 
                 {/* payment method */}
                 <td
-                  className={`border-b border-[var(--border-default)] px-2 py-1.5 uppercase`}
+                  className={`hidden border-b border-[var(--border-default)] px-2 py-1.5 uppercase md:block`}
                 >
                   {txn["paymentMethod"]}
                 </td>
