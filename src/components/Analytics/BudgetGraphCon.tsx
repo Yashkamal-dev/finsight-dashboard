a675657a8a76a4e78c6d1a078ff241460b7f1eb4
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-05-02 17:23:48 +0530

    style(analytics): make budget graph responsive

--- src/components/Analytics/BudgetGraphCon.tsx
+++ src/components/Analytics/BudgetGraphCon.tsx
@@ -24,7 +24,9 @@ const BudgetGraph = () => {
   });
 
   return (
-    <div className={`h-140 w-full`}>
+    <div
+      className={`h-100 w-full -translate-x-3 text-sm md:h-140 md:text-base`}
+    >
       <ResponsiveContainer width="100%" height="100%">
         <BarChart data={chartData}>
           <CartesianGrid strokeDasharray="3 3" />
@@ -51,7 +53,7 @@ const BudgetGraphCon = () => {
   return (
     <div className="flex flex-[1_1_700px] flex-col gap-4 rounded-4xl border border-[var(--border-default)] p-4">
       {/* title of the graph */}
-      <header className="text-2xl font-semibold">
+      <header className="text-xl font-semibold md:text-2xl">
         <h1>Total Budget Overview</h1>
       </header>
 
