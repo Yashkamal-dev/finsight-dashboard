c92d14bd5701cfa53c5414258e880d2070293422
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-05-02 12:40:52 +0530

    style(transactions): improve modal responsiveness and add dropdown transitions

--- src/components/general/AddTransactonForm.tsx
+++ src/components/general/AddTransactonForm.tsx
@@ -171,15 +171,15 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
   };
 
   return (
-    <div className="relative flex w-125 flex-col gap-5 rounded-3xl bg-[var(--bg-primary)] px-5 py-4 shadow-xl">
+    <div className="relative flex w-95 flex-col gap-3 rounded-3xl bg-[var(--bg-primary)] px-4 py-2.5 shadow-xl md:w-125 md:gap-5 md:px-5 md:py-4">
       {/* form heading */}
-      <div className="flex flex-col items-center justify-center gap-1">
+      <div className="flex flex-col items-center justify-center md:gap-1">
         {/* title */}
-        <h1 className="text-2xl font-semibold text-[var(--text-primary)]">
+        <h1 className="text-xl font-semibold text-[var(--text-primary)] md:text-2xl">
           Add Transaction
         </h1>
         {/* subttle */}
-        <h2 className="text- text-[var(--text-secondary)]">
+        <h2 className="text-center text-sm text-[var(--text-secondary)] md:text-base">
           Record your income or expense in seconds.
         </h2>
       </div>
@@ -188,17 +188,17 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
       <form
         action="\"
         onSubmit={(e) => submitHandler(e)}
-        className="flex flex-col gap-5"
+        className="flex flex-col gap-3 md:gap-5"
       >
         {/* type - amount container */}
         <div className="flex h-fit items-center gap-3">
           {/* type */}
           <div className="flex grow flex-col gap-2">
             {/* type label */}
-            <p className="text-lg font-medium">Type</p>
+            <p className="font-medium md:text-lg">Type</p>
 
             {/* button container for types */}
-            <div className="flex gap-3">
+            <div className="flex gap-2 md:gap-3">
               {Types.map((typ) => {
                 return (
                   <button
@@ -206,7 +206,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
                     onClick={() => {
                       setType(typ);
                     }}
-                    className={`cursor-pointer rounded-full text-lg shadow-lg ${type === typ ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} border border-[var(--border-default)] px-3.5 py-1`}
+                    className={`cursor-pointer rounded-full text-sm shadow-lg md:text-base md:text-lg ${type === typ ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} border border-[var(--border-default)] px-3.5 py-1`}
                   >
                     {typ}
                   </button>
@@ -219,10 +219,10 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
           <div className="flex grow flex-col gap-2">
             {/* amount label */}
             <div className="flex items-center gap-2">
-              <p className="text-lg font-medium">Amount</p>
+              <p className="font-medium md:text-lg">Amount</p>
               {errors["amount"] && (
                 // text to show on error - (if empty)
-                <span className="text-lg text-[var(--danger)]">
+                <span className="text-[var(--danger)] md:text-lg">
                   {amount == undefined ? "*" : "Invalid"}
                 </span>
               )}
@@ -239,7 +239,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
                 });
                 return setAmount(Number(e.target.value));
               }}
-              className={`w-full rounded-full border ${errors["amount"] === true ? "border-[var(--danger)]" : ""} border-[var(--border-default)] py-2 pl-3 shadow-lg focus:outline-0`}
+              className={`w-full rounded-full border text-sm transition-all duration-100 ease-in md:text-base ${errors["amount"] === true ? "border-[var(--danger)]" : ""} border-[var(--border-default)] py-1 pl-3 shadow-lg focus:outline-0 md:py-2`}
             />
           </div>
         </div>
@@ -250,7 +250,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
           <div className="flex grow flex-col gap-2">
             {/* name label */}
             <div className="flex items-center gap-2">
-              <p className="text-lg font-medium">Name</p>
+              <p className="font-medium md:text-lg">Name</p>
               {errors["name"] && (
                 // text to show on error - (if empty)
                 <span className="text-lg text-[var(--danger)]">*</span>
@@ -268,14 +268,14 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
                 });
                 setName(e.target.value);
               }}
-              className={`grow rounded-full border ${errors["name"] === true ? "border-[var(--danger)]" : ""} border-[var(--border-default)] py-2 pl-5 text-lg shadow-lg focus:outline-0`}
+              className={`grow rounded-full border text-sm md:text-lg ${errors["name"] === true ? "border-[var(--danger)]" : ""} border-[var(--border-default)] py-2 pl-5 shadow-lg focus:outline-0`}
             />
           </div>
 
           {/* note container */}
           <div className="flex grow flex-col gap-2">
             {/* note label */}
-            <p className="text-lg font-medium"> Note</p>
+            <p className="font-medium md:text-lg"> Note</p>
 
             {/* note input */}
             <textarea
@@ -284,7 +284,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
               onChange={(e) => {
                 setNote(e.target.value);
               }}
-              className="w h-13 grow resize-none rounded-full border border-[var(--border-default)] py-2 pl-5 text-lg shadow-lg focus:outline-0"
+              className="h-10 grow resize-none rounded-full border border-[var(--border-default)] py-2 pl-5 text-sm shadow-lg focus:outline-0 md:h-13 md:text-lg"
             ></textarea>
           </div>
         </div>
@@ -295,7 +295,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
           <div className="flex grow flex-col gap-2.5">
             <div className="flex items-center gap-2">
               {/* method label */}
-              <p className="text-lg font-medium">Method</p>
+              <p className="font-medium md:text-lg">Method</p>
               {errors["method"] && (
                 // text to show on error - (if empty)
                 <span className="text-lg text-[var(--danger)]">*</span>
@@ -321,7 +321,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
           <div className="flex grow flex-col gap-2.5">
             <div className="flex items-center gap-2">
               {/* category method */}
-              <p className="text-lg font-medium">Category</p>
+              <p className="font-medium md:text-lg">Category</p>
               {errors["category"] && (
                 // text to show on error - (if empty)
                 <span className="text-lg text-[var(--danger)]">*</span>
@@ -347,7 +347,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
         {/* Date */}
         <div className="flex flex-col gap-2.5">
           {/* date label */}
-          <p className="text-lg font-medium">Date</p>
+          <p className="font-medium md:text-lg">Date</p>
 
           {/* date option */}
           <div className="flex gap-3">
@@ -356,7 +356,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
               onClick={() => {
                 setDate(new Date());
               }}
-              className={`grow cursor-pointer rounded-full ${isToday ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} border border-[var(--border-default)] px-5 py-1 text-lg shadow-lg`}
+              className={`grow cursor-pointer rounded-full ${isToday ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} border border-[var(--border-default)] py-0.5 text-sm shadow-lg md:px-5 md:py-1 md:text-lg`}
             >
               Today
             </button>
@@ -366,7 +366,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
               onClick={() => {
                 setDate(yesterdayDate);
               }}
-              className={`grow ${isYesterday ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} cursor-pointer rounded-full border border-[var(--border-default)] px-5 py-1 text-lg shadow-lg`}
+              className={`grow ${isYesterday ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} cursor-pointer rounded-full border border-[var(--border-default)] py-0.5 text-sm shadow-lg md:px-5 md:py-1 md:text-lg`}
             >
               Yerterday
             </button>
@@ -378,7 +378,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
               <PopoverTrigger asChild>
                 {/* custom date button */}
                 <button
-                  className={`grow cursor-pointer ${isCustom ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} rounded-full border border-[var(--border-default)] px-5 py-1 text-lg shadow-lg`}
+                  className={`grow cursor-pointer ${isCustom ? "bg-[var(--accent-primary)] text-[var(--text-inverse)]" : "hover:bg-[var(--accent-subtle)]"} rounded-full border border-[var(--border-default)] py-0.5 text-sm shadow-lg md:px-5 md:py-1 md:text-lg`}
                 >
                   {isCustom
                     ? `${date?.toLocaleDateString("en-CA")}`
@@ -402,13 +402,13 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
         </div>
 
         {/* cancel and save button */}
-        <div className="mt-1 flex gap-3 border-t border-[var(--border-default)] pt-4.5 pb-1">
+        <div className="mt-1 flex gap-1.5 border-t border-[var(--border-default)] pt-3 pb-1 md:gap-3 md:pt-4.5">
           {/* cancel button */}
           <button
             onClick={() => {
               setIsAdding(false);
             }}
-            className="grow cursor-pointer rounded-full border border-[var(--border-default)] bg-[var(--bg-primary)] py-2.5 text-[var(--text-primary)] shadow-xl"
+            className="grow cursor-pointer rounded-full border border-[var(--border-default)] bg-[var(--bg-primary)] py-1 text-[var(--text-primary)] shadow-xl md:py-2.5"
           >
             Cancel
           </button>
@@ -416,7 +416,7 @@ const AddTransactonForm = ({ setIsAdding }: prop) => {
           {/* save button */}
           <button
             onClick={saveTransaction}
-            className="grow cursor-pointer rounded-full border border-[var(--border-default)] bg-[var(--accent-primary)] py-2.5 text-[var(--text-inverse)] shadow-xl"
+            className="grow cursor-pointer rounded-full border border-[var(--border-default)] bg-[var(--accent-primary)] py-1 text-[var(--text-inverse)] shadow-xl md:py-2.5"
           >
             Save
           </button>
